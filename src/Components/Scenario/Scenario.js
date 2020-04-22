657bf6c3aec157a8c6f645c86964915ef19601dc
FurlongB <37218765+FurlongB@users.noreply.github.com>
2020-04-22 16:08:31 +0100

    All code added for Scenario screen, only need to make cosmetic changes now

--- src/Components/Scenario/Scenario.js
+++ src/Components/Scenario/Scenario.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
 
 import Video from './Video/Video';
-import Spinner from '../UI/Spinner/Spinner'
+import Options from './QuestionOptions/QuestionOptions';
+import Spinner from '../UI/Spinner/Spinner';
 
 import  {scenario}  from './Scenarios';
 
@@ -9,28 +10,36 @@ import classes from './Scenario.css';
 
 const scenarioQuestions = (props) =>{
     const [nextQ, setNextQ] = useState(1);
-    const [vidToPlay, setVidToPlay] = useState(null)
+    const [vidToPlay, setVidToPlay] = useState(null);
+    const allScenarios = scenario.Sceanarios;
     useEffect(() =>{
-        const allScenarios = scenario.Sceanarios;
         setVidToPlay(allScenarios["Scenario_"+nextQ].video_file)
-        console.log("vidToPlay: ",allScenarios["Scenario_"+nextQ].video_file);
+        //console.log("vidToPlay: ",allScenarios["Scenario_"+nextQ].video_file);
         return () =>{
-            console.log('Clean Up');
+            //console.log('Clean Up');
         }
     }, [nextQ]);
 
+    const setNextScenario = (nQuest) =>{
+        setVidToPlay(null);
+        setNextQ(nQuest);
+    }
+
     return(
-        <div className={classes.ScenarioPanel}>
+        <div className={classes.Ruled}>
             <div className={classes.box}>
-              <h1>{props.pageTitle}</h1>
+                <h1>{props.pageTitle}</h1>
                 Please look at the video and select the next step for the situation.
             </div>
-            {vidToPlay !== null ? <Video vidToPlay={vidToPlay}/>: <Spinner/>}
+            <div>
+                {vidToPlay !== null ? <Video vidToPlay={vidToPlay}/>: <Spinner/>}
+            </div>
+            <div>
+                {vidToPlay !== null ? <Options options={allScenarios["Scenario_"+nextQ].options} submitClick={setNextScenario.bind(this)}/>: null}
+            </div>
         </div>
           
         
     );
 };
-
-
 export default (scenarioQuestions);
--- src/Components/Scenario/Video/Video.js
+++ src/Components/Scenario/Video/Video.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 import classes from './Video.css';
 
-const video = (props)=>(
-  <div className={classes.Video}>
-    <video controls="controls" width="320" height="240" >
-      <source src={require(`../../../assets/scenarios/${props.vidToPlay}`)} type="video/mp4"/>
-      Your browser does not support the HTML5 Video element.
-  </video>
-  </div>    
-);
+const video = (props)=>{
+
+  return(
+    <div className={classes.Video}>
+      <video controls="controls" width="320" height="240" >
+        <source src={require(`../../../assets/scenarios/${props.vidToPlay}`)} type="video/mp4"/>
+        Your browser does not support the HTML5 Video element.
+      </video>
+    </div>  
+  )  
+};
 
 export default video;
