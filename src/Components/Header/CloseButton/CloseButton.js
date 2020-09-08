3825a779caba35571f9da591b3e7b524c1b8176d
FurlongB <37218765+FurlongB@users.noreply.github.com>
2020-09-08 21:45:11 +0100

    Issue with closing window needs to be looked at

--- src/Components/Header/CloseButton/CloseButton.js
+++ src/Components/Header/CloseButton/CloseButton.js
@@ -4,13 +4,17 @@ import CloseIcon from '@material-ui/icons/Close';
 import classes from './CloseButton.css'
 
 const closeButton = () =>{
-    const handleClose = () =>{
-        window.close();
-        console.log('Closing the screen')
+    const handleClose = (cmd) =>{
+        let location = window.location.href
+        if (cmd==='quit')
+        {
+            window.open(location, '_self').close();
+        }   
+        //console.log('Closing the screen')
     }
     return(
         <div className={classes.CloseButton}>
-            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+            <IconButton edge="start" color="inherit" onClick={handleClose.bind(this, "quit")} aria-label="close">
               <CloseIcon />
             </IconButton>
         </div>
